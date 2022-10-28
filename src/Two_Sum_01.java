ba37e8057aecf868276777f3224007a938c1ced8
yumubi <shaman2077c@163.com>
2022-10-28 20:24:43 +0800

    22/10/28/add two answers

--- src/Two_Sum_01.java
+++ src/Two_Sum_01.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Two_Sum_01 {
     public static void main(String[] args) {
@@ -20,5 +21,41 @@ class Solution {
             else return new int[]{Arrays.binarySearch(aux,nums[lo]),Arrays.binarySearch(aux, nums[hi])};
         }
         return new int[]{0,1};
+    }
+}
+
+/**
+ * 暴力枚举
+ */
+class answer1 {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) return new int[]{i, j};
+            }
+        }
+        return new int[]{0,1};
+    }
+}
+
+
+/**
+ * hash表
+ */
+
+/*
+ 两个元素x，y必然是一前一后出现的，如果存在符合条件的解
+ 在遍历到x时，哈希表里没有符合的y，此时把x加入到了哈希表里
+ 当遍历到y时，就可以在哈希表里找到对应的x了，所以只需要一次遍历
+ */
+class answer2 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])
+                return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
