c49faad56e3a847350b60b47f0b3a31e9812fa33
yumubi <shaman2077c@163.com>
2022-10-29 21:35:52 +0800

    回文数09答案

--- src/Palindrome_Number_09/Main.java
+++ src/Palindrome_Number_09/Main.java
@@ -20,3 +20,33 @@ class Solution {
         return true;
     }
 }
+
+
+class answer1 {
+    public boolean isPalindrome(int x) {
+        String reverseStr = (new StringBuilder(x + "")).reverse().toString();
+        return reverseStr.equals(x + "");
+    }
+}
+
+
+/**
+ * 反转一半数字
+ * 时间:O(log n)
+ * 空间: O(1)
+ *
+ */
+
+class answer2 {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || x %10 == 0 && x != 0) return false;
+        int revertedNum = 0;
+        while(x > revertedNum) {
+            revertedNum = revertedNum * 10 + x % 10;
+            x /= 10;
+        }
+
+        //位数为奇数需要除去中位数
+        return x == revertedNum || x == revertedNum / 10;
+    }
+}
