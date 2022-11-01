f2ca38268ee8ac0bd11ff549c496245cf0a6f5db
yumubi <shaman2077c@163.com>
2022-11-01 13:30:00 +0800

    更新分治法

--- src/Longest_Commom_Prefix/Solution.java
+++ src/Longest_Commom_Prefix/Solution.java
@@ -33,7 +33,7 @@ public class Solution {
     }
 
     /**
-     *
+     *startWitch
      */
     public String longestCommonPrefix1(String[] strs) {
         if(strs.length==0)return "";
@@ -49,8 +49,10 @@ public class Solution {
         return s;
     }
 
+
+
     /**
-     *
+     *扫描
      */
     public String longestCommonPrefix2  (String[] strs) {
         String result = strs[0];
@@ -71,12 +73,72 @@ public class Solution {
         return "";
     }
 
+
+
+
+
     /**
-     * todo
+     * 分治法
      */
 
+    public String longestCommonPrefix3(String[] strs) {
+
+        return longestCommonPrefix3(strs, 0, strs.length - 1);
+
+    }
+    public  String longestCommonPrefix3(String[] strs, int lo, int hi) {
+
+        if(lo >= hi) return strs[lo];
+        int mid = lo + (hi - lo) / 2;
+        String lcpLeft = longestCommonPrefix3(strs,lo, mid);
+        String lcpRight = longestCommonPrefix3(strs,mid+1, hi);
+         return commonPrefix(lcpLeft,lcpRight);
+    }
+    public String commonPrefix(String lcpLeft, String lcpRight) {
+            int minLen = Math.min(lcpLeft.length(), lcpRight.length());
+        for (int i = 0; i < minLen; i++) {
+            if(lcpLeft.charAt(i) != lcpRight.charAt(i)) return lcpLeft.substring(0,i);
+        }
+            return lcpLeft.substring(0,minLen);
+    }
 
 
+    /**
+     * todo
+     *二分法
+     */
+
+    public String longestCommonPrefix4(String[] strs) {
+        int minlength = Integer.MAX_VALUE;
+        for(String str : strs) {
+            minlength = Math.min(str.length(),minlength);
+        }
+        int low = 0;
+        int high = minlength;
+        while( low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if(isCommonPrefix(strs,mid)) {
+                low = mid;
+            }  else {
+                high = mid -1;
+            }
+        }
+        return strs[0].substring(0,low);
+    }
+
+    public boolean isCommonPrefix(String[] strs,int length) {
+        String str0 = strs[0].substring(0,length);
+        int count = strs.length;
+        for (int i = 0; i < count; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                if(str0.charAt(i) != str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 
 
