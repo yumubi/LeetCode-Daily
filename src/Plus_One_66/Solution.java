172ffb5811bbc1356393b8c350c891e1a2d5da97
yumubi <shaman2077c@163.com>
2022-11-05 11:30:34 +0800

    官方题解

--- src/Plus_One_66/Solution.java
+++ src/Plus_One_66/Solution.java
@@ -13,7 +13,6 @@ public class Solution {
      * @return
      */
     public int[] plusOne(int[] digits) {
-        if(digits == null || digits.length == 0) return new int[]{0};
         int endPtr = digits.length - 1;
         while(endPtr >= 0 && ++digits[endPtr]  == 10) {
             digits[endPtr] = 0;
@@ -27,6 +26,29 @@ public class Solution {
         }
         return digits;
     }
+
+    /**
+     * 找出最长的后缀9
+     * 逆序遍历，找出第一个不为9的元素加一，并且将其后面的元素置为0
+     * @param digits
+     * @return
+     */
+    public int[] plusOne1(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if(digits[i] != 9) {
+                ++digits[i];
+                for(int j = i + 1; j < n; j++) digits[j] = 0;
+                return digits;
+            }
+        }
+
+        // TODO: 2022/11/5 不太清楚为什么前面不加判断直接return了,后面在new有什么用，不是直接返回了吗
+            int[] ans = new int[n+1];
+            ans[0] = 1;
+            return ans;
+    }
+
 
     @Test
     public void test() {
