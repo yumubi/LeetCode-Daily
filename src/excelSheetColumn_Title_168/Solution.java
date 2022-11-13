103bd9e4f9056577a9ab2fb82cb50fba45b70021
yumubi <shaman2077c@163.com>
2022-11-13 21:50:29 +0800

    assert

--- src/excelSheetColumn_Title_168/Solution.java
+++ src/excelSheetColumn_Title_168/Solution.java
@@ -55,6 +55,31 @@ public class Solution {
         return sb.reverse().toString();
     }
 
+    /**
+     * 这是一道从 1 开始的的 26 进制转换题。
+     * 对于一般性的进制转换题目，只需要不断地对 columnNumber 进行 % 运算取得最后一位，
+     * 然后对 columnNumber 进行 / 运算，将已经取得的位数去掉，直到 columnNumber 为 00 即可。
+     * 一般性的进制转换题目无须进行额外操作，是因为我们是在「每一位数值范围在 [0,x)的前提下进行「逢 x 进一」。
+     * 但本题需要我们将从 1开始，因此在执行「进制转换」操作前，我们需要先对 columnNumber 执行减一操作，从而实现整体偏移
+     * @param cn
+     * @return
+     */
+
+    public String convertToTitle2(int cn) {
+        StringBuilder sb = new StringBuilder();
+        while (cn > 0) {
+            //因为x%26=[0,25] 但是我们取从[1-26]对应[A-Z]
+            // 直接取模 26%26=0 不在我们范围内 所以我们采用-1 [0-25]对应[A-Z] (26-1)%26 =25 25+'A'='Z
+            cn--;
+            sb.append((char)(cn % 26 + 'A'));
+            cn /= 26;
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+
+
+
     @Test
     public void test() {
         int i = 260;
