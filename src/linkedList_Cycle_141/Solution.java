8dd5b986035c940b2cfead2544db088b1e210e2a
yumubi <shaman2077c@163.com>
2022-11-13 16:21:05 +0800

    三叶tql

--- src/linkedList_Cycle_141/Solution.java
+++ src/linkedList_Cycle_141/Solution.java
@@ -74,7 +74,14 @@ public class Solution {
         }
         return true;
     }
-    public boolean hasCycle3p()
+    public boolean hasCycle3p(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
 
 
 
