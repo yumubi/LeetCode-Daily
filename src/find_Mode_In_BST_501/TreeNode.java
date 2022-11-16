26ef300fce7404ac099ca947374858f7db4f5d92
yumubi <shaman2077c@163.com>
2022-11-16 19:39:31 +0800

    为了找到一个众数数组，也太麻烦了，这代码太丑了

--- src/find_Mode_In_BST_501/TreeNode.java
+++ src/find_Mode_In_BST_501/TreeNode.java
@@ -1,9 +1,10 @@
 package src.find_Mode_In_BST_501;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TreeNode {
     int val;
@@ -19,14 +20,28 @@ public class TreeNode {
 
 
     class Solution {
+        /**
+         * 为了找到一个众数数组，也太麻烦了，这代码太丑了
+         * @param root
+         * @return
+         */
           public int[] findMode(TreeNode root) {
-
                 List<Integer> res = new ArrayList<>();
                 preorder(root, res);
-                res.sort((o1, o2) -> o1 - o2);
-
-
-                return new int[]{32};
+              HashMap<Integer, Integer> map = new HashMap<>();
+              for (Integer num: res) {
+                 map.put(num, map.getOrDefault(num, 0)+1);
+              }
+              int max = 0;
+              for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                  if(entry.getValue() > max) max = entry.getValue();
+              }
+              ArrayList<Integer> result = new ArrayList<>();
+              for( Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                  if(entry.getValue() == max)  result.add(entry.getKey());
+              }
+                int[] a = result.stream().mapToInt(Integer :: intValue).toArray();
+              return a;
 
           }
 
@@ -36,5 +51,16 @@ public class TreeNode {
               preorder(root.left, res);
               preorder(root.right, res);
           }
+
+
+
+
+
+
+    }
+
+    @Test
+    public void test() {
+
     }
 }
--- src/happy_Number_202/Solution.java
+++ src/happy_Number_202/Solution.java
@@ -66,11 +66,25 @@ public class Solution {
 
 
     /**
-     * Freud
+     * Freud循环查找算法
+     * 通过反复调用 getNext(n) 得到的链是一个隐式的链表。
+     * 我们不是只跟踪链表中的一个值，而是跟踪两个值，称为快跑者和慢跑者。在算法的每一步中，
+     * 慢速在链表中前进 1 个节点，快跑者前进 2 个节点（对 getNext(n) 函数的嵌套调用）。
+     * 如果 n 是一个快乐数，即没有循环，那么快跑者最终会比慢跑者先到达数字 1。
+     * 如果 n 不是一个快乐的数字，那么最终快跑者和慢跑者将在同一个数字上相遇
+     *
+     * 时间复杂度：O(logn)
+     * 空间复杂度O(1)
      * @param n
      * @return
      */
     public boolean isHappy3(int n) {
+        //双指针有两个注意点，快指针每次眺两个不用担心跳过1的点，
+        // 因为1的点的下一个点还是1。快指针在慢指针后一个位置出发
+        // ，这是因为只要求可追击，不要求环形列表的起始点
+
+        //环形链表龟兔是同一起点即可，但是为什么快乐数同一起点不可以
+        //因为如果是同一起点出发的话，while循环就进不去
         int slowRunner = n;
         int fastRunner = getNext(n);
         while(fastRunner != 1 && slowRunner != fastRunner) {
@@ -79,6 +93,31 @@ public class Solution {
         }
         return fastRunner == 1;
     }
+
+
+    /**
+     * 下一个值可能比自己大的最大数字是什么？根据我们之前的分析，我们知道它必须低于 243。
+     * 因此，我们知道任何循环都必须包含小于 243 的数字，用这么小的数字，编写一个能找到所有周期的强力程序并不困难。
+     * 如果这样做，您会发现只有一个循环： 4→16→37→58→89→145→42→20→4。
+     * 所有其他数字都在进入这个循环的链上，或者在进入 1 的链上。
+     * 因此，我们可以硬编码一个包含这些数字的散列集，如果我们达到其中一个数字，那么我们就知道在循环中。
+     * @param n
+     * @return
+     */
+
+
+    private static Set<Integer> cycleMembers =
+            new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public boolean isHappy4(int n) {
+
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+
 
 
 
