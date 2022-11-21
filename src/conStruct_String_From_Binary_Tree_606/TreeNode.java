39f28f29da509eec9c231b1e57b4ffdb5973c241
yumubi <shaman2077c@163.com>
2022-11-21 15:07:46 +0800

    268

--- src/conStruct_String_From_Binary_Tree_606/TreeNode.java
+++ src/conStruct_String_From_Binary_Tree_606/TreeNode.java
@@ -32,19 +32,19 @@ public class TreeNode {
           }
 
 
-
-
+          /**
+           *
+           * @param root
+           * @return
+           */
           public String tree2str(TreeNode root) {
               if(root == null) return "";
               if(root.left == null && root.right == null) return Integer.toString(root.val);
               if(root.right == null) return new StringBuffer().append(root.val).append("(")
                       .append(tree2str(root.left)).append(")").toString();
-              return new StringBuffer().append(root.val)
+              return new StringBuffer().append(root.val).append("(")
+                      .append(tree2str(root.left)).append(")(").append(tree2str(root.right))
+                      .append(")").toString();
           }
-
-
-
-
-
       }
 }
--- src/missing_Number_268/Solution.java
+++ src/missing_Number_268/Solution.java
@@ -1,7 +1,47 @@
 package src.missing_Number_268;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int missingNumber(int[] nums){
 
+        Arrays.sort(nums);
+        //if(nums[0] != 0) return 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != i) return i;
+        }
+        return nums.length;
     }
+
+    /**
+     * 排序
+     * @param nums
+     * @return
+     */
+    public int missingNumber2(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i) return i;
+        }
+        return n;
+    }
+
+
+    public int missingNumber3(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        int n = nums.length;
+        for(int i = 0; i < n; i++) set.add(nums[i]);
+        int missing = -1;
+        for(int i = 0; i <= n; i++) {
+            if(!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+        return missing;
+    }
+
 }
